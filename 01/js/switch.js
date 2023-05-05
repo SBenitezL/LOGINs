32e179d6497d47c975470e06add142b55edf725b
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-05-05 15:29:08 -0500

    Se culmina con el login 01

--- 01/js/switch.js
+++ 01/js/switch.js
@@ -1,19 +1,32 @@
 
 function cambiar() {
+    console.log('La función ha empezado');
     var signup = document.getElementById("action1");
     var login = document.getElementById("action2");
-
     var sign_move = document.getElementById("sign-up");
     var login_move = document.getElementById("login");
     
-  
-    if (signup.style.display === "none") {
-        login_move.classList.toggle('activo');
-        login.style.display = "none";
-        signup.style.display = "flex";
-    } else {
+    if(signup.style.display === ""){
         signup.style.display = "none";
         login.style.display = "flex";
+    }
+    if (signup.style.display === "none") {
+        login_move.classList.toggle('active');
+        login_move.addEventListener('transitionend', function(event){
+            login.style.display = "none";
+            login_move.classList.remove('active');
+            signup.style.display = "flex";
+        });
+       return;
+    }else {   
+        sign_move.classList.toggle("active");
+        sign_move.addEventListener("transitionend", function(event)
+        {
+            signup.style.display = "none";
+            sign_move.classList.remove("active");
+            login.style.display = "flex"; 
+        });      
+        return;
     }
   }
   
